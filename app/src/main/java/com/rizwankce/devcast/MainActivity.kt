6c71034854b2941f4cebaef6b9cd44a7b13291a0
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2020-06-04 21:27:55 +0530

    adds progress bar for network request

--- app/src/main/java/com/rizwankce/devcast/MainActivity.kt
+++ app/src/main/java/com/rizwankce/devcast/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.rizwankce.devcast
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,7 +14,9 @@ class MainActivity : AppCompatActivity() {
 
     private val networking = NetworkingService()
     private val podcastAdapter = PodcastAdapter()
+    private lateinit var progressBar: ProgressBar
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,13 +24,26 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = podcastAdapter
 
+        progressBar = findViewById(R.id.progressBar)
+        hideProgressBar()
+
         getPodcasts()
     }
 
     private fun getPodcasts() {
+        showProgressBar()
         networking.fetchPoscasts().observe(this, Observer { podcastList ->
             podcastList.forEach { println(it)}
+            hideProgressBar()
             podcastAdapter.update(podcastList)
         })
+    }
+
+    private fun showProgressBar() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        progressBar.visibility = View.GONE
     }
 }
